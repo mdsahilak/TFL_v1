0ff7a863c28c051dac697eea7b377f00c2d4d1ac
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-05-12 20:34:01 +0100

    updates

--- Sources/Dataset/TFLDataset.swift
+++ Sources/Dataset/TFLDataset.swift
@@ -39,29 +39,3 @@ struct TFLDataset {
         
     }
 }
-
-struct TrainTimeInfo: Codable {
-    var line: String { line_.uppercased() }
-    var direction: String { direction_.uppercased() }
-    var departure: String { departure_.uppercased() }
-    var destination: String { destination_.uppercased() }
-    var time: Double { time_ }
-    
-    private var line_: String
-    private var direction_: String
-    private var departure_: String
-    private var destination_: String
-    private var time_: Double
-    
-    var description: String {
-        "\(line) \(direction) \(departure) \(destination) \(time)"
-    }
-    
-    enum CodingKeys: String, CodingKey {
-        case line_ = "line"
-        case direction_ = "direction"
-        case departure_ = "departure"
-        case destination_ = "destination"
-        case time_ = "time"
-    }
-}
