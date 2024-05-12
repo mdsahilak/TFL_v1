9f82246196f14e925bde7b1fea6d66f212edbabd
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-05-12 21:05:13 +0100

    remove unwanted code

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,16 +5,6 @@ import Foundation
 
 print("Welcome to the DSA - TFL v1 Application")
 
-//let graph = Graph(adjacencyList: [
-//    "A": [Edge("B", 4), Edge("C", 9)],
-//    "B": [Edge("A", 4), Edge("C", 5), Edge("D", 9)],
-//    "C": [Edge("A", 9), Edge("B", 5), Edge("C", 7)],
-//    "D": [Edge("C", 7), Edge("B", 9), Edge("E", 2)],
-//    "E": [Edge("D", 2)]
-//])
-//
-//let (distance, path) = graph.findShortestPath(from: "A", to: "E")
-
 TFLDataset.loadData()
 
 while true {
