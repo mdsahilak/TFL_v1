5b5afa09bd35906eff361a0978a41bbc9d5fa676
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-05-13 05:36:12 +0100

    Refactor

--- Sources/TFL_v1.swift
+++ Sources/TFL_v1.swift
@@ -202,34 +202,5 @@ struct TFL_v1 {
     
 }
 
-struct TrackSectionInfo: CustomStringConvertible, Equatable {
-    var start: String
-    var end: String
-    
-    var line: String
-    var direction: String
-    
-    var delay: Double
-    
-    init(start: String, end: String, line: String, direction: String, delay: Double = 0.0) {
-        self.start = start
-        self.end = end
-        self.line = line
-        self.direction = direction
-        self.delay = delay
-    }
-    
-    init(station: String, edge: Edge, delay: Double = 0.0) {
-        self.start = station
-        self.end = edge.destination
-        
-        self.line = edge.line
-        self.direction = edge.direction
-        
-        self.delay = delay
-    }
-    
-    var description: String {
-        "\(line.capitalized.trimmingCharacters(in: .whitespacesAndNewlines)) (\(direction)) : \(start) to \(end) - \(delay) min delay"
-    }
-}
+
+
